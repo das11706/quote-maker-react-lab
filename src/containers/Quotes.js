df4be515531ede50cc565a77d4ade7afa893c0a5
Daniel Suarez <suarez.daniel101@gmail.com>
2023-07-03 10:52:35 -0400

    commit tree 84cfdc4ee2abace153d2401c2a49b6a4dc4c632e

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -4,6 +4,8 @@ import QuoteCard from '../components/QuoteCard';
 
 class Quotes extends Component {
 
+  renderQuotes = () => this.props.quotes.map((quote) => <QuoteCard upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} removeQuote={this.props.removeQuote} key={quote.id} quote={quote} />);
+
   render() {
     return (
       <div>
@@ -15,11 +17,12 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
+              
                 TODO:
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              
+               {this.renderQuotes()}
             </div>
           </div>
         </div>
@@ -29,4 +32,18 @@ class Quotes extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = state => {
+  return {
+    quotes: state.quotes 
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    removeQuote: quote => dispatch({ type: "REMOVE_QUOTE", quote: quote }),
+    upvoteQuote: quote => dispatch({ type: "UPVOTE_QUOTE", quote: quote }),
+    downvoteQuote: quote => dispatch({ type: "DOWNVOTE_QUOTE", quote: quote }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
